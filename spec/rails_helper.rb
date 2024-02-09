6a48b26fc8848563721d0cf55864df3c45c48f27
Hamma Alamin <hamm.alamin@feng.sebhau.edu.ly>
2024-02-09 20:34:43 +0200

    add no-sandbox option

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -115,6 +115,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--no-sandbox")
   options.add_argument("--window-size=1920,1080")
   options.add_argument("use-fake-ui-for-media-stream")
   options.add_argument("use-fake-device-for-media-stream")
